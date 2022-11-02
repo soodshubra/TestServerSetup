164362f1dd7414b237b7911a6728d22695e59a8c
soodshubra <116800903+soodshubra@users.noreply.github.com>
2022-11-02 15:04:41 +0530

    Update config.php

--- CollegeMgmt/application/config/config.php
+++ CollegeMgmt/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://3.86.93.253/CollegeMgmt/';
+$config['base_url'] = 'http://34.229.204.163/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------
